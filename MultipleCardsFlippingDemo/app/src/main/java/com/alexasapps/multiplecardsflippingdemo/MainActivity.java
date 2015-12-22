b99fa1e217a168f0916b31562d80f32eb96fcace
AlexaGh <alexa.ghiurau@gmail.com>
2015-12-22 23:07:18 +0200

    multiple card flipping demo, not working - yet

--- MultipleCardsFlippingDemo/app/src/main/java/com/alexasapps/multiplecardsflippingdemo/MainActivity.java
+++ MultipleCardsFlippingDemo/app/src/main/java/com/alexasapps/multiplecardsflippingdemo/MainActivity.java
@@ -3,24 +3,19 @@ package com.alexasapps.multiplecardsflippingdemo;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int[] imageViewIds = {R.id.imgApples, R.id.imgBannana, R.id.imgKiwi, R.id.imgLemon, R.id.imgLimes, R.id.imgOrange,
-            R.id.imgRaspBerry, R.id.imgStrawberry, R.id.imgWatermelon};
-    int[] backImageIds = {R.id.imgBack, R.id.imgBack1, R.id.imgBack2, R.id.imgBack3, R.id.imgBack4, R.id.imgBack5,
-            R.id.imgBack6, R.id.imgBack7, R.id.imgBack8};
+    ImageView imgFront;// = (ImageView) findViewById(R.id.imgBack1);
+    ImageView imgBack;// = (ImageView) findViewById(R.id.imgApples);
 
-    Button btnFlip;
+    //ImageView[] frontImages = {(ImageView) findViewById(R.id.imgBack1), (ImageView) findViewById(R.id.imgBack2)};
+    //ImageView[] backImages = {(ImageView) findViewById(R.id.imgApples), (ImageView) findViewById(R.id.imgBannana)};
 
     boolean isBackVisible = false;
 
@@ -29,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //imgFront = frontImages[0];
+       // imgBack = backImages[0];
+
+        imgFront = (ImageView)findViewById(R.id.imgApples);
+        imgBack = (ImageView)findViewById(R.id.imgBack1);
 
         final AnimatorSet setRightOut = (AnimatorSet) AnimatorInflater.loadAnimator(getApplicationContext(),
                 R.animator.flip_right_out);
@@ -37,32 +37,28 @@ public class MainActivity extends AppCompatActivity {
         final AnimatorSet setLeftIn = (AnimatorSet) AnimatorInflater.loadAnimator(getApplicationContext(),
                 R.animator.flight_left_in);
 
-        btnFlip.setOnClickListener(new View.OnClickListener() {
+
+        imgFront.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!isBackVisible) {
-                    for (int i = 0; i < imageViewIds.length; i++) {
-                        setRightOut.setTarget(imageViewIds[i]);
-                        setLeftIn.setTarget(backImageIds[i]);
-                        setRightOut.start();
-                        setLeftIn.start();
-                    }
-
+                if(!isBackVisible){
+                    setRightOut.setTarget(imgFront);
+                    setLeftIn.setTarget(imgBack);
+                    setRightOut.start();
+                    setLeftIn.start();
                     isBackVisible = true;
-                } else {
-                    for (int i = 0; i < imageViewIds.length; i++) {
-                        setRightOut.setTarget(backImageIds[5]);
-                        setLeftIn.setTarget(imageViewIds[1]);
-                        setRightOut.start();
-                        setLeftIn.start();
-                    }
-
+                }
+                else{
+                    setRightOut.setTarget(imgBack);
+                    setLeftIn.setTarget(imgFront);
+                    setRightOut.start();
+                    setLeftIn.start();
                     isBackVisible = false;
                 }
             }
         });
-
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
